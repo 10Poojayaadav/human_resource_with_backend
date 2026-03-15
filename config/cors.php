f18cdaf21903dea6a7c5d0a59c297d47c644d45a
Pooja Yadav <110241690+10Poojayaadav@users.noreply.github.com>
2026-03-15 09:44:34 +0530

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -1,20 +1,20 @@
 <?php
 return [
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'], // allow all API routes
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'], // GET, POST, PUT, DELETE, etc.
+    'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'], // React dev server
-    // or '*' to allow all (not recommended for production)
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'], // allow all headers including Authorization
+    'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
 
     'max_age' => 0,
 
-    'supports_credentials' => true, // set true if using cookies/auth
+    'supports_credentials' => false,
+
 ];
